0f3c8c71ae3f977701a426379e2412a8e019f51e
Caia Chuang <caia@Caias-MacBook-Pro.local>
2023-06-19 20:02:31 -0700

    cs50p: week 0 function.

--- hello.py
+++ hello.py
@@ -2,12 +2,8 @@
 name = input("What's your name? ")
 
 # Say hello to user
-<<<<<<< HEAD
-print("hello, world")
-print(name)
-=======
 """
 Multi line comment
 """
 print("Hello,", name)
->>>>>>> 645cacb (cs50p: week 0 function.)
+
